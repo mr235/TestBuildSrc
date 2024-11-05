a7cb00ab4285a943b715f8417e240f6cda79653f
mr235 <mr235z@hotmail.com>
2024-11-05 09:11:17 +0800

    bus register

--- app/src/main/java/com/mr235/testbuildsrc/MainActivity.kt
+++ app/src/main/java/com/mr235/testbuildsrc/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.mr235.testbuildsrc
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private val mHandler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testBusPlugin("hello-world")
-        mHandler.postDelayed({
-            BusUtilsExt.post("testBusPlugin", "hello-world from handler")
-        }, 5000)
+        findViewById<View>(R.id.btPost).setOnClickListener {
+            BusUtilsExt.post("testBusPlugin", "hello-world")
+        }
+        BusUtilsExt.register(this)
     }
 
     @BusUtilsExt.Bus(tag = "testBusPlugin")
     fun testBusPlugin(content: String) {
         println(content)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        BusUtilsExt.unregister(this)
     }
 }
