afd22eb5a1cf80c9e03d07cf4085d7d065541b7d
mr235 <mr235z@hotmail.com>
2025-06-04 15:48:35 +0800

    升级libs版本

--- bus-gradle-plugin-to-gradle-plugin-portal/build.gradle.kts
+++ bus-gradle-plugin-to-gradle-plugin-portal/build.gradle.kts
@@ -64,16 +64,16 @@ tasks.withType<JavaCompile>().configureEach {
 dependencies {
     implementation(gradleApi())
 
-    implementation("com.android.tools.build:gradle:8.1.0")
-    implementation("com.android.tools.build:gradle-api:8.1.0")
+    implementation("com.android.tools.build:gradle:8.10.1")
+    implementation("com.android.tools.build:gradle-api:8.10.1")
 
-    implementation("commons-io:commons-io:2.13.0")
+    implementation("commons-io:commons-io:2.16.1")
     implementation("commons-codec:commons-codec:1.15")
 
-    implementation("org.ow2.asm:asm-commons:9.6")
-    implementation("org.ow2.asm:asm-util:9.7")
-    implementation("org.ow2.asm:asm-tree:9.7")
+    implementation("org.ow2.asm:asm-commons:9.7.1")
+    implementation("org.ow2.asm:asm-util:9.7.1")
+    implementation("org.ow2.asm:asm-tree:9.7.1")
 
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
 }
--- bus-gradle-plugin/build.gradle.kts
+++ bus-gradle-plugin/build.gradle.kts
@@ -41,13 +41,13 @@ dependencies {
     implementation("com.android.tools.build:gradle:8.10.1")
     implementation("com.android.tools.build:gradle-api:8.10.1")
 
-    implementation("commons-io:commons-io:2.13.0")
+    implementation("commons-io:commons-io:2.16.1")
     implementation("commons-codec:commons-codec:1.15")
 
-    implementation("org.ow2.asm:asm-commons:9.6")
-    implementation("org.ow2.asm:asm-util:9.7")
-    implementation("org.ow2.asm:asm-tree:9.7")
+    implementation("org.ow2.asm:asm-commons:9.7.1")
+    implementation("org.ow2.asm:asm-util:9.7.1")
+    implementation("org.ow2.asm:asm-tree:9.7.1")
 
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
 }
